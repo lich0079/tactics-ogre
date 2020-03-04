686cb5e283baefb2dc0dd7405386c09a58da0bff
lich0079 <lich0079@gmail.com>
2020-03-04 14:37:49 +0800

    add youtube link

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-appcfg.sh  --proxy=den-entbc-001:80  update //Users/zhayang/Dropbox/exchange/tactics-ogre1995/war
+appcfg.sh  --proxy=den-entbc-001:80  update /Users/zhayang/Dropbox/exchange/tactics-ogre/war
